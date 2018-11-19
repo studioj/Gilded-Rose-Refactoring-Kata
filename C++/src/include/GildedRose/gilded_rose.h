c2292b706e713ce6dc643839a0a24f5e9543a5fd
clayhill <roeland.vanlembergen@gmail.com>
2018-11-19 14:33:16 +0100

    Add C++ variant of the kata

--- C++/src/include/GildedRose/gilded_rose.h
+++ C++/src/include/GildedRose/gilded_rose.h
@@ -1,4 +1,19 @@
 #ifndef GILDEDROSE_GILDED_ROSE_H
 #define GILDEDROSE_GILDED_ROSE_H
 
+#include <string>
+
+class GildedRose
+{
+    std::string _name;
+    int _days_remaining;
+    int _quality;
+
+public:
+    GildedRose(const std::string &name, int days_remaining, int quality);
+    void tick();
+    int days_remaining();
+    int quality();
+};
+
 #endif //GILDEDROSE_GILDED_ROSE_H
